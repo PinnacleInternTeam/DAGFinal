183ae4f4d7b47b493e2e21009c320062fe0bd9d1
rolin <devteamro@gmail.com>
2023-02-14 12:50:10 +0530

    table changes

--- src/components/home1.jsx
+++ src/components/home1.jsx
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Icon from '../assets/dag_logo1.png';
+import refresh from '../assets/download.png';
 import '../components/CSS/home1.css'
 import Headersuper from './Layout/Headersuper';
 
@@ -33,7 +33,35 @@ export default function Home1() {
 
         </div>
         <div className="col-lg-10 col-md-12 col-sm-12 col-xs-12 text-left"><br /><br />
-        <h1 style={{ fontFamily: "Serif", color: "#877bae" }} className="font-weight-bold ">Dashboard</h1>
+        <h1 style={{ fontFamily: "Serif", color: "#877bae" }} className="font-weight-bold ">Dashboard</h1><br/>
+
+        {/* <button><img src={refresh} alt="my image" style={{border:"none"}} /></button> */}
+
+        <div className='text-right'>
+             <img
+                className="img_icon_size log text-right"
+                // onClick={() => onClickReset()}
+                src={refresh}
+                alt="refresh"
+                title="Refresh"
+              />
+              </div>
+       
+            
+        <table border="1">
+            <tr>
+                <th>Org Name</th>
+                <th>Email</th>
+                <th>Phone No.</th>
+                <th>Start Date</th>
+                <th>End Date</th>
+                <th>Status</th>
+                <th>Operation</th>
+            </tr>
+            <tr>
+                
+            </tr>
+        </table>
         </div>
         <div className="col-lg-1 col-md-12 col-sm-12 col-xs-12 text-left">
         </div>
